7974f2c1fcb6cc2ef4c3096eff8dc0d890ab5722
NAZIRwill29 <59091291+NAZIRwill29@users.noreply.github.com>
2022-02-09 09:11:55 +0800

    inline style style in tag and outside tag

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+//custom style to be used outide tag
+const customStyle = {
+  color: "green",
+  fontSize: "5rem",
+  border: "3px solid black"
+};
+//custom style can change with js
+customStyle.color = "blue";
+
+ReactDOM.render(
+  <div>
+    {/* inline css in tag */}
+    <h1 style={{ color: "red" }}>Hello World!</h1>
+    {/* inline css outside tag */}
+    <h1 style={customStyle}>Hello World!</h1>
+  </div>,
+  document.getElementById("root")
+);
